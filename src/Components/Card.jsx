df3a3cac834cf9aee162beddcb024aaa4a903bd5
Darksied95 <aliyuefe9501@gmail.com>
2022-03-11 23:03:43 +0100

    Adding files

---
+++ src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = () =>{
+    return(
+        <div>
+        <h1 className="tc">Search GitHub User</h1>
+        <div className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">Hello</div>
+        </div>
+    )
+
+}
+
+export default Card;
